66cc83afded2df2133f17fab67a2d317b85c0813
stefanspringer1 <stefan.springer@icloud.com>
2024-11-02 14:24:02 +0100

    Initial commit

---
+++ Sources/main.swift
@@ -0,0 +1,39 @@
+let data = """
+    URLs:
+    http://www.google.com
+    https://www.google.com
+    http://google.com
+    https://google.com
+    http://www.google.com/
+    https://www.google.com/
+    http://google.com/
+    https://google.com/
+    http://www.google.com/index.html
+    https://www.google.com/index.html
+    http://google.com/index.html
+    https://google.com/index.html
+    """
+
+print("With characters:")
+print(measure(text: data, regex: #/https?:\/\/([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?/#))
+
+print("\nWith codepoints:")
+print(measure(text: data, regex:#/https?:\/\/([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?/#.matchingSemantics(.unicodeScalar)))
+
+func measure(text: String, regex: Regex<(Substring, Substring, Substring, Substring?)>) -> Duration {
+    print("tic...", terminator: "")
+    
+    let clock = ContinuousClock()
+    
+    var count = 0
+    let result = clock.measure {
+        for _ in 1...10_000 {
+            for _ in data.matches(of: regex) {
+                //print(text[match.range.lowerBound..<match.range.upperBound])
+                count += 1
+            }
+        }
+    }
+    print(" toc (\(count) matches).")
+    return result
+}
